05dbae3e10c5770e4e233b10def4d94ef72d240b
Michael Su <msu@atlassian.com>
2020-11-28 23:09:12 +1100

    update cognito config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_ek7czenWN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2ldlvvte88lmps6jvm0uj3qvqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_ljESOaDRb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '557ruoqhkspm4978t4o5l2pmrl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
